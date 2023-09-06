45c8177c39859f0f2ae8a859736a643f4a145207
inikonzs <zhanzs@mail.ru>
2023-09-06 16:48:10 +0200

    animate round block

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -7,7 +7,7 @@ export function App(){
     const [isStarted, setStarted] = useState(false);
     const [initialTime, setInitialTime] = useState(0);
     const [timerValue, setTimerValue] = useState(0);
-    const roundTime = 10000;
+    const roundTime = 1000;
     useEffect(()=>{
         if (!isStarted) return;
 
@@ -25,7 +25,7 @@ export function App(){
     }, [isStarted]);
 
     return <div className="app_wrapper">
-        <GameView isStarted={isStarted}></GameView>
+        <GameView isStarted={isStarted} initPosition={0}></GameView>
         <div className="app_bottom">
             <StartButton 
                 onClick={()=>{
--- client/src/components/game_view.tsx
+++ client/src/components/game_view.tsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./game_view.css";
 
 interface IGameViewProps {
-    isStarted: boolean
+    isStarted: boolean;
+    initPosition: number;
 }
 
-export function GameView({ isStarted }:IGameViewProps){
-    return <div className="game_wrapper">
+export function GameView({ isStarted}:IGameViewProps){
+    const blockRef = useRef<HTMLDivElement>();
+    const wrapperRef = useRef<HTMLDivElement>();
+    const [initPosition, setInitPosition] = useState(0);
+    
+    useEffect(()=>{
+        const wrapperBounds = wrapperRef.current.getBoundingClientRect();
+        const blockBounds = blockRef.current.getBoundingClientRect();
+        const relTop = ((blockBounds.top - wrapperBounds.height / 2) / blockBounds.height * 100);
+        console.log(wrapperBounds.height, blockBounds.top, relTop);
+        setInitPosition(relTop);
+    },[isStarted]);
+    return <div className="game_wrapper" ref={wrapperRef}>
         {isStarted ? (
              ''
         ) : ''}
         <>
-            <div className="game_block1">
+            <div className="game_block1" ref={blockRef}>
                 1
             </div>
             <div className="game_block2">
                 2
             </div>
-            <div className="game_round">
+            {isStarted && <div className="game_round" style={{'--init-position': initPosition + '%'}}>
                 r
-            </div>
+            </div>}
         </>  
     </div>
 }
