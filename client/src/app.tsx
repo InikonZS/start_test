2adc07301138e8a55230fb7d042e8b0808990c08
inikonzs <zhanzs@mail.ru>
2023-09-06 16:16:59 +0200

    animate blocks

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {GameView} from "./components/game_view";
 import {StartButton} from "./components/start_button";
+import "./style.css";
 
 export function App(){
     const [isStarted, setStarted] = useState(false);
@@ -23,18 +24,21 @@ export function App(){
         }
     }, [isStarted]);
 
-    return <div>
-        <StartButton 
-            onClick={()=>{
-                if (!isStarted){
-                    console.log('start')
-                    setStarted(true);
-                    setInitialTime(Date.now());
-                }
-            }}
-            isStarted={isStarted}
-            time={new Date(timerValue)}
-        ></StartButton>
+    return <div className="app_wrapper">
         <GameView isStarted={isStarted}></GameView>
+        <div className="app_bottom">
+            <StartButton 
+                onClick={()=>{
+                    if (!isStarted){
+                        console.log('start')
+                        setStarted(true);
+                        setInitialTime(Date.now());
+                    }
+                }}
+                isStarted={isStarted}
+                time={new Date(timerValue)}
+            ></StartButton>
+        </div>
+        
     </div>
 }
--- client/src/components/game_view.tsx
+++ client/src/components/game_view.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import "./game_view.css";
 
 interface IGameViewProps {
     isStarted: boolean
 }
 
 export function GameView({ isStarted }:IGameViewProps){
-    return <div>
-        {isStarted ? 'game view' : 'click start'}
+    return <div className="game_wrapper">
+        {isStarted ? (
+             ''
+        ) : ''}
+        <>
+            <div className="game_block1">
+                1
+            </div>
+            <div className="game_block2">
+                2
+            </div>
+            <div className="game_round">
+                r
+            </div>
+        </>  
     </div>
 }
